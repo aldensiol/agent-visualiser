ab7029b77a75080ef8bb86c522cb82fa9d370936
aldensiol <aldensio@live.com>
2024-08-28 13:26:01 +0800

    feat: Add boilerplate code to notebooks and prompts

--- src/ui/components.py
+++ src/ui/components.py
@@ -2,5 +2,5 @@ import streamlit as st
 
 def display_chat():
     st.title("RAG Chatbot")
-    user_input = st.text_input("Ask a question:")
+    user_input = st.text_input("Ask a question:", key="user_input")
     return user_input
--- streamlit_app.py
+++ streamlit_app.py
@@ -1,11 +1,25 @@
 import streamlit as st
+import numpy as np
+import pandas as pd
 
 from src.ui.components import display_chat
 
 def main():
     user_input = display_chat()
     if user_input:
-        st.write("im gay")
+        pass
+    
+    # Add a selectbox to the sidebar:
+    add_selectbox = st.sidebar.selectbox(
+        'How would you like to be contacted?',
+        ('Email', 'Home phone', 'Mobile phone')
+    )
 
+    # Add a slider to the sidebar:
+    add_slider = st.sidebar.slider(
+        'Select a range of values',
+        0.0, 100.0, (25.0, 75.0)
+    )
+    
 if __name__ == "__main__":
     main()
