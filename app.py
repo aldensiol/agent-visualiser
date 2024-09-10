d888a0ab484405561091f86be45ca5a53f16e22b
aldensiol <aldensio@live.com>
2024-09-10 16:48:53 +0800

    update: Edit notebooks

--- app.py
+++ app.py
@@ -1,28 +1,37 @@
 import chainlit as cl
 
+from src.chatbot.agents import AgentGraph
+
 @cl.set_starters
 async def set_starters():
     return [
         cl.Starter(
-            label="Morning routine ideation",
-            message="Can you help me create a personalized morning routine that would help increase my productivity throughout the day? Start by asking me about my current habits and what activities energize me in the morning.",
-            icon="/public/idea.svg",
-            ),
-
-        cl.Starter(
-            label="Explain superconductors",
-            message="Explain superconductors like I'm five years old.",
-            icon="/public/learn.svg",
+            label="Explain what AI Agents are",
+            message="Explain what AI Agents are like I'm five years old.",
+            icon="src/icons/agent.svg",
             ),
         cl.Starter(
-            label="Python script for daily email reports",
-            message="Write a script to automate sending daily email reports in Python, and walk me through how I would set it up.",
-            icon="/public/terminal.svg",
+            label="GraphRag over VectorRag",
+            message="In what scenarios is GraphRag better than VectorRag?",
+            icon="src/icons/exercise.svg",
             ),
         cl.Starter(
-            label="Text inviting friend to wedding",
-            message="Write a text asking a friend to be my plus-one at a wedding next month. I want to keep it super short and casual, and offer an out.",
-            icon="/public/write.svg",
+            label="Exercises for diabetics",
+            message="Please suggest some appropriate exercises for diabetics with heart conditions.",
+            icon="src/icons/nodes.svg",
             )
         ]
-# ...
+    
+@cl.on_chat_start
+async def on_chat_start():
+    # Set Agent Chain Here
+    cl.user_session.set("graph", AgentGraph)
+
+@cl.on_message
+async def on_message(message: cl.Message):
+    chain = cl.user_session.get("graph")
+    res = await chain.ainvoke(
+        question=message.content, callbacks=[cl.LangchainCallbackHandler()]
+    )
+
+    await cl.Message(content=res).send()
