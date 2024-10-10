e2895f600549e00bef83e9963c892dbaab2ae38f
aldensiol <aldensio@live.com>
2024-10-10 16:31:58 +0800

    feat: Add icons to frontend interface

--- app.py
+++ app.py
@@ -6,19 +6,19 @@ import chainlit as cl
 async def set_starters():
     return [
         cl.Starter(
-            label="Explain what AI Agents are",
-            message="Explain what AI Agents are like I'm five years old.",
-            icon="src/icons/agent.svg",
-            ),
-        cl.Starter(
-            label="GraphRag over VectorRag",
-            message="In what scenarios is GraphRag better than VectorRag?",
-            icon="src/icons/nodes.svg",
+            label="Medicines for Diabetes",
+            message="What are some Medicines for Diabetes?",
+            icon="https://www.svgrepo.com/show/109551/pill.svg",
             ),
         cl.Starter(
             label="Exercises for diabetics",
             message="Please suggest some appropriate exercises for diabetics with heart conditions.",
-            icon="src/icons/exercise.svg",
+            icon="https://cdni.iconscout.com/illustration/premium/thumb/exercise-illustration-download-in-svg-png-gif-file-formats--dumbbells-fitness-exercises-with-biceps-workout-doing-artistry-pack-people-illustrations-5295079.png",
+            ),
+        cl.Starter(
+            label="Diabetes Management Plan",
+            message="Can you provide a comprehensive weekly plan for a newly diagnosed type 2 diabetic who is also overweight, has high blood pressure, and a family history of kidney disease? Include dietary recommendations, exercise routines, blood sugar monitoring schedule, potential medications, and lifestyle changes. Also, explain how this plan might need to be adjusted as the patient ages or if complications arise.",
+            icon="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRtc4e90qSf97_dpcWCXKHDGI4wuUnmLnnmWg&s",
             )
         ]
     
